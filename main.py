c8d1c93e6a5b2ab239af53f9477ca50bf742e8cf
Arthur <arthur.pointeau@gmail.com>
2021-08-02 00:52:08 +0200

    bar plot prescriptions

--- main.py
+++ main.py
@@ -5,11 +5,22 @@ import matplotlib.pyplot as plt
 
 #pd.read_csv("covid19casos short.csv")["fecha_fallecimiento"].value_counts().plot()
 
+
+
 df = pd.read_csv("fallecimientos.csv")["fecha_fallecimiento"]
 df = pd.to_datetime(df)
 # df.sort_values(by='fecha_fallecimiento')
-df.value_counts().plot()
+#df.value_counts().plot()
+
+
+prescriptions = pd.read_csv("Argentine - Quantité région.csv")
+prescriptions.loc[prescriptions["STATE"] == "LA PAMPA"]
+prescriptions = prescriptions.iloc[: , 1:] #drop first column
+prescriptions.columns = pd.to_datetime(prescriptions.columns)
+prescriptions.iloc[0].plot.bar()
 plt.show()
+
+
 
 # to remove all non deaths
 # df = pd.read_csv("Covid19Casos.csv")
