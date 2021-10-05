4c00cfb6918ce40e3a8fae45d9f7a17ba8bcb283
Arthur <arthur.pointeau@gmail.com>
2021-10-05 13:51:16 +0200

    main

--- main.py
+++ main.py
@@ -5,19 +5,23 @@ import matplotlib.pyplot as plt
 
 #pd.read_csv("covid19casos short.csv")["fecha_fallecimiento"].value_counts().plot()
 
-
-# df = pd.read_csv("fallecimientos.csv")["fecha_fallecimiento"]
+#
+# df = pd.read_csv("casos confirmados La Pampa.csv")["fecha_apertura"]
 # df = pd.to_datetime(df)
-# df.sort_values(by='fecha_fallecimiento')
-#df.value_counts().plot()
-
+# df.sort_values()
+# df.value_counts().plot()
+#
+# df = pd.read_csv("casos confirmados La Pampa.csv")["fallecido"]
+# df = pd.to_datetime(df)
+# df.sort_values()
+# df.value_counts().plot()
 #
 # prescriptions = pd.read_csv("Argentine - Quantité région.csv")
 # prescriptions.loc[prescriptions["STATE"] == "LA PAMPA"]
 # prescriptions = prescriptions.iloc[: , 1:] #drop first column
 # prescriptions.columns = pd.to_datetime(prescriptions.columns)
 # prescriptions.iloc[0].plot.bar()
-# plt.show()
+plt.show()
 #
 
 
@@ -27,9 +31,9 @@ import matplotlib.pyplot as plt
 # # df.to_csv("fallecimientos.csv")
 
 # keep confirmed cases only
-dataframe = pd.read_csv("Covid19Casos.csv")
-for region in dataframe["residencia_provincia_nombre"].unique():
-  region_frame = dataframe[dataframe["residencia_provincia_nombre"] == region]
-  region_frame = region_frame[region_frame["clasificacion_resumen"] == "Confirmado"]
-  region_frame.to_csv("casos confirmados " + region + ".csv")
-  #clasificacion_resumen confirmado y fallecido == no
+dataframe = pd.read_csv("covid19casos.csv")
+region = "Santa Cruz"
+# for region in dataframe["carga_provincia_nombre"].unique():
+region_frame = dataframe[dataframe["carga_provincia_nombre"] == region]
+region_frame = region_frame[region_frame["clasificacion_resumen"] == "Confirmado"]
+region_frame.to_csv("casos confirmados " + region + ".csv")
