4132f4f8bcdd7b09f7cd49721fd9c481705cf55d
Arthur Pointeau <arthur.pointeau@gmail.com>
2021-12-29 18:14:53 +0100

    cleaner filtering

--- CFRxMonthxRegion.py
+++ CFRxMonthxRegion.py
@@ -19,13 +19,14 @@ for region in dataframe["carga_provincia_nombre"].unique(): #for each region
   dates_dict = dict()
 
   for date in pd.date_range("2020-02-01", periods=15, freq="M"): #for each month in the specified range
-    region_frame = dataframe[dataframe["carga_provincia_nombre"] == region] #filter by region
-    region_date_frame = region_frame[region_frame["fecha_apertura"].between(date, date+pd.DateOffset(months = 1))] #filter by date
 
-    datetime = pd.to_datetime("2020-03-05")
-    region_date_frame_confirmed = region_date_frame[region_frame["clasificacion_resumen"] == "Confirmado"]  # filter only confirmed
-    confirmed_cases = region_date_frame_confirmed.shape[0]
-    deaths = region_date_frame_confirmed[region_date_frame_confirmed["fecha_fallecimiento"].notna()].shape[0]
+    filtered_frame = dataframe.loc[(dataframe["carga_provincia_nombre"] == region) &
+                              (dataframe["fecha_apertura"].between(date, date + pd.DateOffset(months=1))) &
+                              (dataframe["clasificacion_resumen"] == "Confirmado") &
+                              (dataframe["edad"] > 65)]
+
+    confirmed_cases = filtered_frame.shape[0]
+    deaths = filtered_frame[filtered_frame["fecha_fallecimiento"].notna()].shape[0]
     if(confirmed_cases > 0): CFR = deaths/confirmed_cases
     dates_dict[date] = CFR
     print("CFR between " + str(date) + " and " + str(date + pd.DateOffset(months=1)) + " in " + region + " is " + str(CFR))
