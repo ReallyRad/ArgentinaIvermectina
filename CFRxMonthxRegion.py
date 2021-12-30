6f31ab802913ced47df51ec2eba3943e3cf38fd2
Arthur Pointeau <arthur.pointeau@gmail.com>
2021-12-29 21:25:54 -0500

    scatterplot for over 65 year olds

--- CFRxMonthxRegion.py
+++ CFRxMonthxRegion.py
@@ -7,6 +7,15 @@ import pandas as pd
 #pour chaque mois
 #divide confirmed cases by death counts (with time delay?)
 
+def simplify(text):
+	import unicodedata
+	try:
+		text = unicode(text, 'utf-8')
+	except NameError:
+		pass
+	text = unicodedata.normalize('NFD', text).encode('ascii', 'ignore').decode("utf-8")
+	return str(text)
+
 dataframe = pd.read_csv("data/Covid19Casos.csv")
 start_date = "2020-04-01"
 
@@ -34,7 +43,15 @@ for region in dataframe["carga_provincia_nombre"].unique(): #for each region
   cfrs_region_month[region] = dates_dict
   cfrs_region_month_df = pd.DataFrame.from_dict(cfrs_region_month, orient="index")
   cfrs_region_month_df.index.name = "Region"
-  cfrs_region_month_df.to_csv(region + ".csv")
-  print("saved " + region + ".csv" )
 
-pd.DataFrame.from_dict(cfrs_region_month, orient="index").to_csv("regions.csv")
+final_df = pd.DataFrame.from_dict(cfrs_region_month, orient="index")\
+
+final_df = pd.read_csv("data/cfr_per_month_per_region_over_65.csv")
+final_df.rename(columns={'Region': 'STATE'}, inplace=True)
+final_df.STATE = final_df.STATE.replace("CABA", "CAPITAL FEDERAL")
+final_df.STATE = [simplify(x).upper() for x in final_df.STATE]
+#cfr_per_region_per_month.columns = [pd.to_datetime(x, errors='ignore').strftime("%M/%Y") for x in cfr_per_region_per_month.columns]
+final_df.sort_values(by = "STATE")
+
+
+final_df.to_csv("cfr_per_month_per_region.csv")
--- ScatterplotxMonthxRegion.py
+++ ScatterplotxMonthxRegion.py
@@ -4,7 +4,6 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 import scipy.stats
 
-
 # Polynomial Regression
 def polyfit(x, y, degree):
     results = {}
@@ -26,7 +25,8 @@ def polyfit(x, y, degree):
     return results
 
 ivm_per_1000_per_region_per_month = pd.read_csv("data/IVMx1000.csv")
-cfr_per_region_per_month = pd.read_csv("data/regions.csv")
+
+cfr_per_region_per_month = pd.read_csv("data/cfr_per_month_per_region_over_65.csv")
 
 ivm_array = []
 cfr_array = []
@@ -35,6 +35,7 @@ for month in ivm_per_1000_per_region_per_month.columns:
     if month != "STATE" and month in cfr_per_region_per_month.columns:
         ivm_array.extend(ivm_per_1000_per_region_per_month[month].values)
         cfr_array.extend(cfr_per_region_per_month[month].values)
+        scatter = plt.scatter(ivm_per_1000_per_region_per_month[month].values, cfr_per_region_per_month[month].values)
 
 #scatter = plt.scatter(ivm_array, cfr_array)
 
@@ -42,16 +43,13 @@ ivm_cfr_df = pd.DataFrame({'ivm':np.array(ivm_array), 'cfr':np.array(cfr_array)}
 ivm_cfr_df = ivm_cfr_df[ivm_cfr_df["cfr"]!=0]
 
 r, p = scipy.stats.pearsonr(np.log(ivm_cfr_df["ivm"]), np.log(ivm_cfr_df["cfr"]))
-#r, p = scipy.stats.pearsonr(np.log(np.array(ivm_array)), np.log(np.array(cfr_array)))
-fitting = polyfit(ivm_array, cfr_array, 2)
 
-scatter = plt.scatter(ivm_cfr_df["ivm"], ivm_cfr_df["cfr"])
 
 plt.xlabel("IVM x 1000 inhabitants")
 plt.ylabel("CFR")
 plt.xscale('log')
 plt.yscale('log')
-plt.title("CFR x Achats IVM en Argentine par région, a partir de Juillet 2020 "
+plt.title("CFR x IVM Purchases, between"
           + 'Pearson R = '+ str(r) + ', p = ' + str(p) )
 plt.show()
 
