71ca18e48b6181ad75f61d17a7a36401d43e686f
Arthur Pointeau <arthur.pointeau@gmail.com>
2021-12-31 20:22:24 -0500

    plot ivm/case, with case as size

--- ScatterplotxMonthxRegion.py
+++ ScatterplotxMonthxRegion.py
@@ -1,33 +1,15 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import scipy.stats
-import seaborn as sns
 import plotly_express as px
 # Polynomial Regression
-def polyfit(x, y, degree):
-    results = {}
-
-    coeffs = np.polyfit(x, y, degree)
-
-    # Polynomial Coefficients
-    results['polynomial'] = coeffs.tolist()
-
-    # r-squared
-    p = np.poly1d(coeffs)
-    # fit values, and mean
-    yhat = p(x)                         # or [p(z) for z in x]
-    ybar = np.sum(y)/len(y)          # or sum(y)/len(y)
-    ssreg = np.sum((yhat-ybar)**2)   # or sum([ (yihat - ybar)**2 for yihat in yhat])
-    sstot = np.sum((y - ybar)**2)    # or sum([ (yi - ybar)**2 for yi in y])
-    results['determination'] = ssreg / sstot
-
-    return results
+import utils
 
 ivm_per_1000_per_region_per_month = pd.read_csv("data/IVMx1000.csv")
 
 cfr_per_region_per_month = pd.read_csv("data/cfr_per_month_per_region.csv")
+cases_per_region_per_month_per_1000 = pd.read_csv("data/cases_per_month_per_region_per_1000_inhabitants.csv")
 
 merged = dict()
 merged_row = 0
@@ -35,22 +17,26 @@ for index, ivm_row in ivm_per_1000_per_region_per_month.iterrows():
     for row_index, ivm_row_value in ivm_row.items():
         if row_index != "STATE" and row_index != "05/2021":
             cfr_value = cfr_per_region_per_month.loc[cfr_per_region_per_month["STATE"] == ivm_row["STATE"]][row_index].values[0]
-            merged[merged_row] = {"STATE": ivm_row["STATE"], "MONTH": row_index, "IVM": ivm_row_value, "CFR": cfr_value}
+            cases_per_1000_value = cases_per_region_per_month_per_1000.loc[cases_per_region_per_month_per_1000["STATE"] == ivm_row["STATE"]][row_index].values[0]
+            merged[merged_row] = {"STATE": ivm_row["STATE"], "MONTH": row_index, "IVM": ivm_row_value, "CFR": cfr_value, "CASES": cases_per_1000_value}
             merged_row += 1
 
 merged_df = pd.DataFrame.from_dict(merged, orient="index")
+
+merged_df["IVM/CASE"] = merged_df["IVM"]/merged_df["CASES"]
 
 ivm_cfr_df = pd.DataFrame({'ivm':np.array(merged_df["IVM"]), 'cfr':np.array(merged_df["CFR"])})
 ivm_cfr_df = ivm_cfr_df[ivm_cfr_df["cfr"]!=0]
 
 r, p = scipy.stats.pearsonr(np.log(ivm_cfr_df["ivm"]), np.log(ivm_cfr_df["cfr"]))
 
-fig = px.scatter(merged_df, x="IVM",
+fig = px.scatter(merged_df, x="IVM/CASE",
                  y="CFR",
                  title="CFR x IVM Purchases, " + 'Pearson R = '+ str(r) + ', p = ' + str(p),
                  color="MONTH",
                  hover_data=["MONTH", "STATE"],
                  log_x=True,
-                 log_y=True)
+                 log_y=True,
+                 size="CASES")
 
 fig.show()
