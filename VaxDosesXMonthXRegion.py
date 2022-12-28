43e221240793b272d18791cbcc8cdf31444f01ff
Arthur Pointeau <arthur.pointeau@gmail.com>
2022-12-28 22:54:52 +0100

    new doses per 1000 inhabitants

--- VaxDosesXMonthXRegion.py
+++ VaxDosesXMonthXRegion.py
@@ -9,29 +9,32 @@ def simplify(text):
 	text = unicodedata.normalize('NFD', text).encode('ascii', 'ignore').decode("utf-8")
 	return str(text)
 
-
-#vax_df = pd.read_csv("data/datos_nomivac_covid19.csv")
-vax_df = pd.read_csv("data/datos_nomivac_covid19.csv")
+vax_df = pd.read_csv("data/datos_nomivac_covid19.csv", usecols = ["fecha_aplicacion", "jurisdiccion_residencia", ])
 vax_region_month = dict()
+population_df = pd.read_csv("data/population_region.csv")
 
 vax_df["fecha_aplicacion"] = pd.to_datetime(vax_df["fecha_aplicacion"])
 
 for region in vax_df["jurisdiccion_residencia"].unique():
   dates_dict = dict()
+  if region == 'CABA':
+    region = 'CAPITAL FEDERAL'
+  elif region != "S.I.":
+    for date in pd.date_range("2020-02-01", periods=15, freq="M"):
+      filtered_frame = vax_df.loc[(vax_df["jurisdiccion_residencia"] == region) &
+                                  (vax_df["fecha_aplicacion"].between(date, date + pd.DateOffset(months=1)))]
 
-  for date in pd.date_range("2020-02-01", periods = 15, freq="M"):
-    filtered_frame = vax_df.loc[(vax_df["jurisdiccion_residencia"] == region) &
-                                (vax_df["fecha_aplicacion"].between(date, date + pd.DateOffset(months=1)))]
+      new_vaccine_doses = filtered_frame.shape[0]
 
-    new_vaccine_doses = filtered_frame.shape[0]
+      dates_dict[date] = new_vaccine_doses
 
-    dates_dict[date] = new_vaccine_doses
+      print("region : " + str(region))
+      print("period : from " + str(date) + " to " + str(date + pd.DateOffset(months=1)))
+      print("new vaccine doses : " + str(new_vaccine_doses))
+      population = population_df[population_df.STATE == simplify(region).upper()]["Population"].values[0]
+      dates_dict[date] = new_vaccine_doses / population * 1000
 
-    print("region : " + str(region))
-    print("period : from " + str(date) + " to " + str(date + pd.DateOffset(months=1)))
-    print("new vaccine doses : " + str(new_vaccine_doses))
-
-    vax_region_month[region] = dates_dict
+      vax_region_month[region] = dates_dict
 
 final_df = pd.DataFrame.from_dict(vax_region_month, orient="index")
 final_df = final_df.reset_index()
@@ -40,4 +43,4 @@ final_df.STATE = final_df.STATE.replace("CABA", "CAPITAL FEDERAL")
 final_df.STATE = [simplify(x).upper() for x in final_df.STATE]
 #cfr_per_region_per_month.columns = [pd.to_datetime(x, errors='ignore').strftime("%M/%Y") for x in cfr_per_region_per_month.columns]
 
-final_df.to_csv("data/vaxXMonthXRegion.csv")
+final_df.to_csv("data/vaxXMonthXRegionX1000.csv")
